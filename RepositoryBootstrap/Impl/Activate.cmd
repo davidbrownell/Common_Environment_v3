5fc1ea18f0b597e5aa7a46a99fdbf44a02154278
David Brownell <db@DavidBrownell.com>
2018-05-16 22:52:42 -0700

    Added Activate scripts

---
+++ RepositoryBootstrap/Impl/Activate.cmd
@@ -0,0 +1,233 @@
+@REM ----------------------------------------------------------------------
+@REM |  
+@REM |  Activate.cmd
+@REM |  
+@REM |  David Brownell <db@DavidBrownell.com>
+@REM |      2018-05-03 16:38:31
+@REM |  
+@REM ----------------------------------------------------------------------
+@REM |  
+@REM |  Copyright David Brownell 2018.
+@REM |  Distributed under the Boost Software License, Version 1.0.
+@REM |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+@REM |  
+@REM ----------------------------------------------------------------------
+@echo off
+
+pushd %~dp0
+
+set _ACTIVATE_ENVIRONMENT_PREVIOUS_FUNDAMENTAL=%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%
+
+REM Read the bootstrap data
+if not exist "%~dp0Generated\Windows\EnvironmentBootstrap.data" (
+    @echo.
+    @echo ERROR: It appears that Setup.cmd has not been run for this repository. Please run Setup.cmd and run this script again.
+    @echo.
+    @echo        [%~dp0Generated\Windows\EnvironmentBootstrap.data was not found]
+    @echo.
+
+    goto ErrorExit
+)
+
+for /f "tokens=1,2 delims==" %%a in (%~dp0Generated\Windows\EnvironmentBootstrap.data) do (
+    if "%%a"=="fundamental_repo" set DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL=%%~fb
+    if "%%a"=="is_tool_repo" set _ACTIVATE_ENVIRONMENT_IS_TOOL_REPOSITORY=%%b
+    if "%%a"=="is_configurable" set _ACTIVATE_ENVIRONMENT_IS_CONFIGURABLE_REPOSITORY=%%b
+)
+
+REM Find the python binary
+for /f "tokens=*" %%G in ('dir /b /a:d "%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%\Tools\Python\v*"') do (
+    if exist "%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%\Tools\Python\%%G\Windows\python.exe" (
+        set _ACTIVATE_ENVIRONMENT_PYTHON_BINARY="%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%\Tools\Python\%%G\Windows\python.exe"
+    )
+)
+
+set PYTHONPATH=%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%
+
+@REM ----------------------------------------------------------------------
+@REM |  List configurations if requested
+if "%1" NEQ "ListConfigurations" goto :AfterListConfiguration
+
+REM Get the remaining args
+set _ACTIVATE_ENVIRONMENT_WORKING_DIR=%~dp0
+set _ACTIVATE_ENVIRONMENT_CLA=
+
+shift
+
+:GetRemainingArgs_ListConfigurations
+if "%1" NEQ "" (
+    set _ACTIVATE_ENVIRONMENT_CLA=%_ACTIVATE_ENVIRONMENT_CLA% %1
+    shift
+    goto :GetRemainingArgs_ListConfigurations
+)
+
+%_ACTIVATE_ENVIRONMENT_PYTHON_BINARY% -m RepositoryBootstrap.Impl.Activate ListConfigurations %_ACTIVATE_ENVIRONMENT_WORKING_DIR% %_ACTIVATE_ENVIRONMENT_CLA%
+goto Exit
+
+:AfterListConfiguration
+
+@REM ----------------------------------------------------------------------
+@REM |  Indicate if this is a tool repo if requested
+if "%1" NEQ "IsToolRepository" goto :AfterIsToolRepository
+
+REM Get the remaining args
+set _ACTIVATE_ENVIRONMENT_WORKING_DIR=%~dp0
+set _ACTIVATE_ENVIRONMENT_CLA=
+
+shift
+
+:GetRemainingArgs_IsToolRepository
+if "%1" NEQ "" (
+    set _ACTIVATE_ENVIRONMENT_CLA=%_ACTIVATE_ENVIRONMENT_CLA% %1
+    shift
+    goto :GetRemainingArgs_IsToolRepository
+)
+
+%_ACTIVATE_ENVIRONMENT_PYTHON_BINARY% -m RepositoryBootstrap.Impl.Activate IsToolRepository %_ACTIVATE_ENVIRONMENT_WORKING_DIR% %_ACTIVATE_ENVIRONMENT_CLA%
+goto Exit
+
+:AfterIsToolRepository
+
+@REM If here, we are in a verified activation scenario. Set the previous value to this value, knowing that that is the value
+@REM that will be committed.
+set _ACTIVATE_ENVIRONMENT_PREVIOUS_FUNDAMENTAL=%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%
+
+@REM ----------------------------------------------------------------------
+@REM |  Only allow one activated environment at a time (unless we are activating a tool repo)
+if "%_ACTIVATE_ENVIRONMENT_IS_TOOL_REPOSITORY%" NEQ "1" if "%DEVELOPMENT_ENVIRONMENT_REPOSITORY%" NEQ "" if /i "%DEVELOPMENT_ENVIRONMENT_REPOSITORY%\" NEQ "%~dp0" (
+    @echo.
+    @echo ERROR: Only one repository can be activated within an environment at a time, and it appears as if one is already active. Please open a new console and run this script again.
+    @echo.
+    @echo        [DEVELOPMENT_ENVIRONMENT_REPOSITORY is already defined as "%DEVELOPMENT_ENVIRONMENT_REPOSITORY%"]
+    @echo.
+
+    goto :ErrorExit
+)
+
+@REM ----------------------------------------------------------------------
+@REM |  A tool repository can't be activated in isolation
+if "%_ACTIVATE_ENVIRONMENT_IS_TOOL_REPOSITORY%"=="1" if "%DEVELOPMENT_ENVIRONMENT_REPOSITORY_ACTIVATED_FLAG%" NEQ "1" (
+    @echo.
+    @echo ERROR: A tool repository cannot be activated in isolation. Activate another repository before activating this one.
+    @echo.
+
+    goto :ErrorExit
+)
+
+@REM ----------------------------------------------------------------------
+@REM |  Prepare the args
+if "%_ACTIVATE_ENVIRONMENT_IS_CONFIGURABLE_REPOSITORY%" NEQ "0" (
+    if "%1" == "" (
+        @echo.
+        @echo ERROR: This repository is configurable, which means that it can be activated in a variety of different ways. Please run this script again with a configuration name provided on the command line.
+        @echo.
+        @echo        Available configurations are:
+        @echo.
+        %_ACTIVATE_ENVIRONMENT_PYTHON_BINARY% -m RepositoryBootstrap.Impl.Activate ListConfigurations %~dp0 command_line
+        @echo.
+    
+        goto :ErrorExit
+    )
+    
+    if "%DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION%" NEQ "" (
+        if "%DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION%" NEQ "%1" (
+            @echo.
+            @echo ERROR: The environment was previously activated with this repository but using a different configuration. Please open a new console window and activate this repository with the new configuration.
+            @echo.
+            @echo        ["%DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION%" != "%1"]
+            @echo.
+        
+            goto :ErrorExit
+        )
+    )
+    
+    set _ACTIVATE_ENVIRONMENT_CLA=%*
+    goto :AfterClaArgsSet
+)
+
+set _ACTIVATE_ENVIRONMENT_CLA=None %*
+
+:AfterClaArgsSet
+
+REM Create a temporary file that contains output produced by the python script. This lets us quickly bootstrap
+REM to the python environment while still executing OS-specific commands.
+call :CreateTempScriptName
+
+REM Generate...
+%_ACTIVATE_ENVIRONMENT_PYTHON_BINARY% -m RepositoryBootstrap.Impl.Activate Activate "%_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME%" %~dp0 %_ACTIVATE_ENVIRONMENT_CLA%
+set _ACTIVATE_ENVIRONMENT_SCRIPT_GENERATION_ERROR_LEVEL=%ERRORLEVEL%
+
+REM Invoke...
+if exist "%_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME%" (
+    call %_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME%
+)
+set _ACTIVATE_ENVIRONMENT_SCRIPT_EXECUTION_ERROR_LEVEL=%ERRORLEVEL%
+
+REM Process errors...
+if "%_ACTIVATE_ENVIRONMENT_SCRIPT_GENERATION_ERROR_LEVEL%" NEQ "0" (
+    @echo.
+    @echo ERROR: Errors were encountered and the environment has not been successfully activated for development.
+    @echo.
+    @echo        [%DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL%\RepositoryBootstrap\Impl\Activate.py failed]
+    @echo.
+
+    goto :ErrorExit
+)
+
+if "%_ACTIVATE_ENVIRONMENT_SCRIPT_EXECUTION_ERROR_LEVEL%" NEQ "0" (
+    @echo.
+    @echo ERROR: Errors were encountered and the environment has not been successfully activated for development.
+    @echo.
+    @echo        [%_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME% failed]
+    @echo.
+
+    goto :ErrorExit
+)
+
+REM Cleanup
+del "%_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME%
+
+@echo                                                 ^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^
+@echo                                                 ^<                                                                                           ^>
+@echo                                                 ^>   The environment has been activated for this repository and is ready for development.    ^<
+@echo                                                 ^<                                                                                           ^>
+@echo                                                 ^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^v^^
+@echo.
+@echo.
+
+set _ACTIVATE_ERROR_LEVEL=0
+goto Exit
+
+@REM ----------------------------------------------------------------------
+:ErrorExit
+set _ACTIVATE_ERROR_LEVEL=-1
+goto :Exit
+
+@REM ----------------------------------------------------------------------
+:Exit
+set DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL=%_ACTIVATE_ENVIRONMENT_PREVIOUS_FUNDAMENTAL%
+
+set _ACTIVATE_ENVIRONMENT_SCRIPT_EXECUTION_ERROR_LEVEL=
+set _ACTIVATE_ENVIRONMENT_SCRIPT_GENERATION_ERROR_LEVEL=
+set _ACTIVATE_ENVIRONMENT_CLA=
+set _ACTIVATE_ENVIRONMENT_WORKING_DIR=
+set _ACTIVATE_ENVIRONMENT_PYTHON_BINARY=
+set _ACTIVATE_ENVIRONMENT_IS_CONFIGURABLE_REPOSITORY=
+set _ACTIVATE_ENVIRONMENT_IS_TOOL_REPOSITORY=
+set _ACTIVATE_ENVIRONMENT_PREVIOUS_FUNDAMENTAL=
+set _ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME=
+set PYTHONPATH=
+
+popd 
+
+exit /B %_ACTIVATE_ERROR_LEVEL%
+
+@REM ---------------------------------------------------------------------------
+:CreateTempScriptName
+setlocal EnableDelayedExpansion
+set _filename=%~dp0ActivateEnvironment-!RANDOM!-!Time:~6,5!.cmd
+endlocal & set _ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME=%_filename%
+
+if exist "%_ACTIVATE_ENVIRONMENT_TEMP_SCRIPT_NAME%" goto :CreateTempScriptName
+goto :EOF
+@REM ---------------------------------------------------------------------------
---
+++ RepositoryBootstrap/Impl/Activate.sh
@@ -0,0 +1,267 @@
+# ----------------------------------------------------------------------
+# |  
+# |  Activate.sh
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2018-05-11 16:53:08
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2018.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+set +v                                      # Disable output
+
+# Note that we can't exit or return from this script, as it is invoked via a short cut at the
+# repo's root. Because of this, we use the ugly 'should_continue hack.
+should_continue=1
+changed_dir=0
+
+# Ensure that the script is being invoked via source ( as it modifies the current environment).
+if [[ $should_continue == 1 && ${0##*/} == Activate.sh ]]
+then
+    echo ""
+    echo "ERROR: This script activates a console for development according to information specific to the repository."
+    echo ""
+    echo "       Because this process makes changes to environment variables, it must be run within the current context. To do this, please source (run) the script as follows:"
+    echo ""
+    echo "          source ./Activate.sh"
+    echo ""
+    echo "              - or -"
+    echo ""
+    echo "          . ./Activate.sh"
+    echo ""
+    echo ""
+
+    should_continue=0
+fi
+
+previous_fundamental=$DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL
+
+if [[ $should_continue == 1 ]]
+then
+    # Get the repo_root, which will be the the directory for the name of the script who links to
+    # this script as its target. Note that this code doesn't work on RedHat; when on RedHat the 
+    # script must be invoked from the repo root.
+    
+    distro_name=`uname -a | tr "[:upper:]" "[:lower:]"`
+    
+    if [[ $distro_name != *el5* ]]
+    then
+        GetCurrentDir()
+        {
+            current_dir="${BASH_SOURCE[0]}";
+            if [ -h "${SCRIPT_PATH}" ] 
+            then
+                while [ -h "${SCRIPT_PATH}" ] 
+                do 
+                    SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; 
+                done
+            fi
+            pushd . > /dev/null
+            cd `dirname ${current_dir}` > /dev/null
+            current_dir=`pwd`;
+            popd  > /dev/null
+        }
+        
+        GetCurrentDir
+        pushd $current_dir > /dev/null                                      # +repo_dir
+        changed_dir=1
+    fi
+fi
+
+# Read data created during setup
+if [[ $should_continue == 1 && ! -e `pwd`/Generated/Linux/EnvironmentBootstrap.data ]]
+then
+    echo ""
+    echo "ERROR: It appears that Setup.sh has not been run for this repository. Please run Setup.sh and run this script again."
+    echo ""
+    echo "       [`pwd`/Generated/Linux/EnvironmentBootstrap.data was not found]"
+    echo ""
+
+    should_continue=0
+fi
+
+# Parse the bootstrap info, extracting the relevant info
+if [[ $should_continue == 1 ]]
+then
+    while read line;
+    do
+        if [[ $line == fundamental_repo* ]]
+        then
+            export DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL=`readlink -f ${line#fundamental_repo=}`
+        elif [[ $line == is_tool_repo* ]]
+        then
+            is_tool_repo=${line#is_tool_repo=}
+        elif [[ $line == is_configurable* ]]
+        then
+            is_configurable=${line#is_configurable=}
+        fi
+
+    done < "`pwd`/Generated/Linux/EnvironmentBootstrap.data"
+fi
+
+# Find the python binary
+python_dir=$DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL/Tools/Python
+pushd $python_dir > /dev/null                                               # +python_dir
+
+for d in $(find v* -maxdepth 0 -type d);
+do
+    if [[ -e $python_dir/$d/Linux/bin/python ]]
+    then
+        python_binary=$python_dir/$d/Linux/bin/python
+    fi
+done
+
+popd > /dev/null                                                            # -python_dir
+
+export PYTHONPATH=$DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL
+
+# ----------------------------------------------------------------------
+# |  List configurations if requested
+if [[ "$1" == "ListConfigurations" ]]
+then
+    shift 1
+
+    $python_binary -m RepositoryBootstrap.Impl.Activate ListConfigurations "`pwd`" "$@"
+    should_continue=0
+fi
+
+# ----------------------------------------------------------------------
+# |  Indicate if this is a tool repo if requested
+if [[ "$1" == "IsToolRepository" ]]
+then
+    shift 1
+    
+    $python_binary -m RepositoryBootstrap.Impl.Activate IsToolRepository "`pwd`" "$@"
+    should_continue=0
+fi
+
+# If here, we are in a verified activation scenario. Set the previous value to this value, knowing that that is the value
+# that will be committed.
+previous_fundamental=$DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL
+ 
+# ----------------------------------------------------------------------
+# |  Only allow one activated environment at a time (unless we are activating a tool repo)
+if [[ $should_continue == 1 && "$DEVELOPMENT_ENVIRONMENT_REPOSITORY" != "" && "$DEVELOPMENT_ENVIRONMENT_REPOSITORY" != "`pwd`" ]]
+then
+    echo ""
+    echo "ERROR: Only one repository can be activated within an environment at a time, and it appears as if one is already active. Please open a new console and run this script again."
+    echo ""
+    echo "       [DEVELOPMENT_ENVIRONMENT_REPOSITORY is already defined as \"$DEVELOPMENT_ENVIRONMENT_REPOSITORY\"]"
+    echo ""
+    
+    should_continue=0
+fi
+
+# ----------------------------------------------------------------------
+# |  A tool repository can't be activated in isolation
+if [[ $should_continue == 1 && $is_tool_repo == "1" && "$DEVELOPMENT_ENVIRONMENT_REPOSITORY_ACTIVATED_FLAG" != "1" ]]
+then
+    echo ""
+    echo "ERROR: A tool repository cannot be activated in isolation. Activate another repository before activating this one."
+    echo ""
+    echo ""
+    
+    should_continue=0
+fi
+
+# ----------------------------------------------------------------------
+# |  Prepare the args
+if [[ $should_continue == 1 ]]
+then
+    if [[ $is_configurable == "1" ]]
+    then
+        if [[ "$1" == "" ]]
+        then
+            echo ""
+            echo "ERROR: This repository is configurable, which means that it can be activated in a variety of different ways. Please run this script again with a configuration name provided on the command line."
+            echo ""
+            echo "       Available configurations are:"
+            echo ""
+            $python_binary -m RepositoryBootstrap.Impl.Activate ListConfigurations `pwd` command_line
+            echo ""
+            echo ""
+            
+            should_continue=0
+            
+        fi
+        
+        if [[ "$DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION" != "" && "$DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION" != "$1" ]]
+        then
+            echo ""
+            echo "ERROR: The environment was previously activated with this repository but using a different configuration. Please open a new console window and activate this repository with the new configuration."
+            echo ""
+            echo "       ["$DEVELOPMENT_ENVIRONMENT_REPOSITORY_CONFIGURATION" != "$1"]"
+            echo ""
+            
+            should_continue=0
+        fi
+    
+        command_line_args="$@"
+    else
+        command_line_args="None \"$@\""
+    fi
+fi
+
+# Generate...
+if [[ $should_continue == 1 ]]
+then
+    temp_script_name=`mktemp`
+    rm $temp_script_name
+    
+    $python_binary -m RepositoryBootstrap.Impl.Activate Activate $temp_script_name "`pwd`" $command_line_args
+    generation_error=$?
+    
+    if [[ -e $temp_script_name ]]
+    then
+        chmod u+x $temp_script_name
+        source $temp_script_name
+    fi
+    execution_error=$?
+    
+    if [[ $generation_error != 0 ]]
+    then
+        echo ""
+        echo "ERROR: Errors were encountered and the environment has not been successfully activated for development."
+        echo ""
+        echo "       [$DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL/RepositoryBootstrap/Impl/Activate.py failed]"
+        echo ""
+        
+        should_continue=0
+        
+    elif [[ $execution_error != 0 ]]
+    then
+        echo ""
+        echo "ERROR: Errors were encountered and the environment has not been successfully activated for development."
+        echo ""
+        echo "       [$temp_script_name failed]"
+        echo ""
+        
+        should_continue=0
+    fi
+fi
+
+# Cleanup
+[[ ! -f $temp_script_name ]] || rm -f "$temp_script_name"
+
+if [[ $should_continue == 1 ]]
+then
+    echo "                                                ^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^"
+    echo "                                                <                                                                                           >"
+    echo "                                                >   The environment has been activated for this repository and is ready for development.    <"
+    echo "                                                <                                                                                           >"
+    echo "                                                ^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^v^"
+    echo ""
+    echo ""
+fi
+
+unset PYTHONPATH
+export DEVELOPMENT_ENVIRONMENT_FUNDAMENTAL=$previous_fundamental
+
+if [[ $changed_dir == 1 ]]
+then
+    popd > /dev/null                                                        # -repo_dir
+fi
