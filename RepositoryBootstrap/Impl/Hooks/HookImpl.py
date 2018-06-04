ff691d09c7a799937e734437d9bbe810d51ec5bc
David Brownell <db@DavidBrownell.com>
2018-06-04 16:52:04 -0700

    Refactored common hooks code into HooksImpl.py

---
+++ RepositoryBootstrap/Impl/Hooks/HookImpl.py
@@ -0,0 +1,187 @@
+# ----------------------------------------------------------------------
+# |  
+# |  HookImpl.py
+# |  
+# |  David Brownell <db@DavidBrownell.com>
+# |      2018-06-04 07:58:48
+# |  
+# ----------------------------------------------------------------------
+# |  
+# |  Copyright David Brownell 2018.
+# |  Distributed under the Boost Software License, Version 1.0.
+# |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# |  
+# ----------------------------------------------------------------------
+"""Implements functionality common to all hooks"""
+
+import json
+import os
+import sys
+
+# ----------------------------------------------------------------------
+_script_fullpath = os.path.abspath(__file__) if "python" in sys.executable.lower() else sys.executable
+_script_dir, _script_name = os.path.split(_script_fullpath)
+# ----------------------------------------------------------------------
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+from RepositoryBootstrap import GetFundamentalRepository as _GetFundamentalRepository
+sys.path.pop(0)
+
+# ----------------------------------------------------------------------
+def Invoke(repo_root, output_stream, method, json_content, is_debug):
+    fundamental_root = _GetFundamentalRepository()
+    
+    # Imports here can be tricky
+    try:
+        sys.path.insert(0, fundamental_root)
+
+        from RepositoryBootstrap import Constants
+        from RepositoryBootstrap.Impl import CommonEnvironmentImports
+
+        sys.path.pop(0)
+    except:
+        import traceback
+
+        output_stream.write(traceback.format_exc())
+        raise
+
+    output_stream = CommonEnvironmentImports.StreamDecorator(output_stream)
+
+    output_stream.write("Getting configurations...")
+    with output_stream.DoneManager() as dm:
+        # Is this a tool repo?
+        bootstrap_filename = os.path.join(repo_root, Constants.GENERATED_DIRECTORY_NAME, CommonEnvironmentImports.CurrentShell.CategoryName, Constants.GENERATED_BOOTSTRAP_JSON_FILENAME)
+        if os.path.isfile(bootstrap_filename):
+            with open(bootstrap_filename) as f:
+                bootstrap_data = json.load(f)
+
+            if bootstrap_data["is_tool_repo"]:
+                repo_root = fundamental_root
+
+        activation_script = os.path.join(repo_root, CommonEnvironmentImports.CurrentShell.CreateScriptName(Constants.ACTIVATE_ENVIRONMENT_NAME))
+        if not os.path.isfile(activation_script):
+            output_stream.write("ERROR: The filename '{}' was not found.\n".format(activation_script))
+            return -1
+
+        result, output = CommonEnvironmentImports.Process.Execute("{} ListConfigurations json".format(activation_script))
+        assert result == 0, output
+
+        data = json.loads(output)
+
+        configurations = list(data.keys())
+        if not configurations:
+            configurations = [ None, ]
+
+    # Process the configurations
+    output_stream.write("Processing configurations...")
+    with output_stream.DoneManager( suffix='\n',
+                                  ) as dm:
+        display_sentinel = "Display?!__"
+
+        json_filename = CommonEnvironmentImports.CurrentShell.CreateTempFilename(".json")
+        with open(json_filename, 'w') as f:
+            json.dump(json_content, f)
+
+        with CommonEnvironmentImports.CallOnExit(lambda: os.remove(json_filename)):
+            original_environment = None
+
+            if os.getenv(Constants.DE_REPO_GENERATED_NAME):
+                # This code sucks because it is hard coding names and duplicating logic in Activate.py. However, importing
+                # Activate here is causing problems as the Mercurial version of python is different enough from out
+                # version that some imports don't work between python 2 and python 3.
+                original_data_filename = os.path.join(os.getenv(Constants.DE_REPO_GENERATED_NAME), "EnvironmentActivation.OriginalEnvironment.json")
+                assert os.path.isfile(original_data_filename), original_data_filename
+
+                with open(original_data_filename) as f:
+                    original_environment = json.load(f)
+
+
+            terminate = False
+
+            for index, configuration in enumerate(configurations):
+                dm.stream.write("Configuration '{}' ({} of {})...".format( configuration if configuration else "<default>",
+                                                                           index + 1,
+                                                                           len(configurations),
+                                                                         ))
+                with dm.stream.DoneManager() as this_dm:
+                    if terminate:
+                        continue
+
+                    result_filename = CommonEnvironmentImports.CurrentShell.CreateTempFilename()
+
+                    # ----------------------------------------------------------------------
+                    def RemoveResultFilename():
+                        if os.path.isfile(result_filename):
+                            os.remove(result_filename)
+
+                    # ----------------------------------------------------------------------
+
+                    with CommonEnvironmentImports.CallOnExit(RemoveResultFilename):
+                        # We are potentially operating in a limited environment. Rather than
+                        # attempting to invoke hook functionality here, activate an environment
+                        # and run the functionality there.
+                        commands = [ CommonEnvironmentImports.CurrentShell.Commands.EchoOff(),
+                                     CommonEnvironmentImports.CurrentShell.Commands.Raw('cd "{}"'.format(os.path.dirname(activation_script))),
+                                     CommonEnvironmentImports.CurrentShell.Commands.Call("{} {} /fast".format(os.path.basename(activation_script), configuration if configuration else '')),
+                                     CommonEnvironmentImports.CurrentShell.Commands.ExitOnError(-1),
+                                     CommonEnvironmentImports.CurrentShell.Commands.Augment("PYTHONPATH", fundamental_root, update_memory=False),
+                                     CommonEnvironmentImports.CurrentShell.Commands.Raw('python -m RepositoryBootstrap.Impl.Hooks.HookScript "{method}" "{sentinel}" "{json_filename}" "{result_filename}"{first}' \
+                                                                                            .format( method=method,
+                                                                                                     sentinel=display_sentinel,
+                                                                                                     json_filename=json_filename,
+                                                                                                     result_filename=result_filename,
+                                                                                                     first=" /first" if index == 0 else '',
+                                                                                                   )),
+                                     CommonEnvironmentImports.CurrentShell.Commands.ExitOnError(-1),
+                                   ]
+
+                        script_filename = CommonEnvironmentImports.CurrentShell.CreateTempFilename(CommonEnvironmentImports.CurrentShell.ScriptExtension)
+                        with open(script_filename, 'w') as f:
+                            f.write(CommonEnvironmentImports.CurrentShell.GenerateCommands(commands))
+
+                        with CommonEnvironmentImports.CallOnExit(lambda: os.remove(script_filename)):
+                            CommonEnvironmentImports.CurrentShell.MakeFileExecutable(script_filename)
+
+                            content = []
+
+                            # ----------------------------------------------------------------------
+                            def Display(value):
+                                if value.startswith(display_sentinel):
+                                    stripped_value = value.replace(display_sentinel, '')
+
+                                    this_dm.stream.write(stripped_value)
+                                    this_dm.stream.flush()
+
+                                content.append(value)
+
+                            # ----------------------------------------------------------------------
+
+                            this_dm.result = CommonEnvironmentImports.Process.Execute( CommonEnvironmentImports.CurrentShell.DecorateInvokeScriptCommandLine(script_filename),
+                                                                                       Display,
+                                                                                       line_delimited_output=True,
+                                                                                       environment=original_environment,
+                                                                                     )
+
+                            if is_debug:
+                                this_dm.stream.write(''.join(content))
+
+                            if this_dm.result == -1:
+                                return this_dm.result
+
+                            if not os.path.isfile(result_filename):
+                                raise Exception("The filename '{}' should have been generated by 'RepositoryBootstrap.Impl.Hooks.HookScript' but it doesn't exist.".format(result_filename))
+
+                            with open(result_filename) as f:
+                                result = int(f.read().strip())
+
+                            if result == -1:
+                                this_dm.result = result
+                                return this_dm.result
+                            elif result == 1:
+                                pass                    # 1 is returned if a configuration was used
+                            elif result == 0:
+                                terminate = True        # 0 is returned if a configuration was not used
+                            else:
+                                assert False, result
+
+    return 0
--- RepositoryBootstrap/Impl/Hooks/Mercurial.py
+++ RepositoryBootstrap/Impl/Hooks/Mercurial.py
@@ -69,7 +69,8 @@ def PreTxnCommit(ui, repo, node, parent1, parent2, *args, **kwargs):
                          kwargs,
                        ))
 
-    return HookImpl.Invoke( ui,
+    return HookImpl.Invoke( _GetRepoRoot(),
+                            ui,
                             "Commit",
                             _GetChangeInfo(repo, repo[node]),
                             is_debug,
@@ -106,7 +107,8 @@ def PreOutgoing(ui, repo, source, *args, **kwargs):
     if "url" in kwargs:
         data["url"] = kwargs["url"]
 
-    return HookImpl.Invoke( ui,
+    return HookImpl.Invoke( _GetRepoRoot(),
+                            ui,
                             "Push",
                             data,
                             is_debug,
@@ -158,7 +160,8 @@ def PreTxnChangeGroup(ui, repo, source, node, node_last=None, *args, **kwargs):
 
     changes.sort(key=lambda c: c["date"])
 
-    return HookImpl.Invoke( ui,
+    return HookImpl.Invoke( _GetRepoRoot(),
+                            ui,
                             "Pull",
                             { "changes" : changes,
                             },
@@ -199,162 +202,17 @@ def _GetChangeInfo(repo, ctx):
              "removed" : [ TransformFilename(filename) for filename in status.removed ],
            }
 
-# BugBug # ----------------------------------------------------------------------
-# BugBug def _Impl(ui, verb, json_content, is_debug):
-# BugBug     # Imports here can be tricky
-# BugBug     try:
-# BugBug         sys.path.insert(0, _fundamental_root)
-# BugBug 
-# BugBug         from RepositoryBootstrap import Constants
-# BugBug         from RepositoryBootstrap.Impl import CommonEnvironmentImports
-# BugBug         
-# BugBug         del sys.path[0]
-# BugBug     except:
-# BugBug         import traceback
-# BugBug 
-# BugBug         ui.write(traceback.format_exc())
-# BugBug         raise
-# BugBug 
-# BugBug     shell = CommonEnvironmentImports.CurrentShell
-# BugBug     
-# BugBug     # Get the configuration to use during environment activation
-# BugBug     output_stream = CommonEnvironmentImports.StreamDecorator(ui)
-# BugBug     
-# BugBug     output_stream.write('\n')
-# BugBug     output_stream.write("Getting configurations...")
-# BugBug     with output_stream.DoneManager() as dm:
-# BugBug         activation_root = os.getcwd()
-# BugBug 
-# BugBug         # Is the repo a tool repo?
-# BugBug         bootstrap_filename = os.path.join(activation_root, Constants.GENERATED_DIRECTORY_NAME, shell.CategoryName, Constants.GENERATED_BOOTSTRAP_JSON_FILENAME)
-# BugBug         if os.path.isfile(bootstrap_filename):
-# BugBug             with open(bootstrap_filename) as f:
-# BugBug                 bootstrap_data = json.load(f)
-# BugBug 
-# BugBug             if bootstrap_data["is_tool_repo"]:
-# BugBug                 # Set the root to the fundamental repo
-# BugBug                 activation_root = _fundamental_root
-# BugBug 
-# BugBug         activation_script = os.path.join(activation_root, shell.CreateScriptName(Constants.ACTIVATE_ENVIRONMENT_NAME))
-# BugBug         if not os.path.isfile(activation_script):
-# BugBug             return 0
-# BugBug 
-# BugBug         result, output = CommonEnvironmentImports.Process.Execute("{} ListConfigurations json".format(activation_script))
-# BugBug         assert result == 0, output
-# BugBug         
-# BugBug         data = json.loads(output)
-# BugBug         
-# BugBug         configurations = list(data.keys())
-# BugBug         if not configurations:
-# BugBug             configurations = [ "None", ]
-# BugBug 
-# BugBug     # Process the configurations
-# BugBug     output_stream.write("Processing configurations...")
-# BugBug     with output_stream.DoneManager( suffix='\n',
-# BugBug                                   ) as dm:
-# BugBug         display_sentinel = "Display?!__"
-# BugBug         
-# BugBug         json_filename = shell.CreateTempFilename(".json")
-# BugBug 
-# BugBug         with open(json_filename, 'w') as f:
-# BugBug             json.dump(json_content, f)
-# BugBug 
-# BugBug         terminate = False
-# BugBug 
-# BugBug         with CommonEnvironmentImports.CallOnExit(lambda: os.remove(json_filename)):
-# BugBug             original_environment = None
-# BugBug 
-# BugBug             if os.getenv(Constants.DE_REPO_GENERATED_NAME):
-# BugBug                 # This code sucks because it is hard coding names and duplicating logic in Activate.py. However, importing
-# BugBug                 # Activate here is causing problems as the Mercurial version of python is different enough from out
-# BugBug                 # version that some imports don't work between python 2 and python 3.
-# BugBug                 original_data_filename = os.path.join(os.getenv(Constants.DE_REPO_GENERATED_NAME), "EnvironmentActivation.OriginalEnvironment.json")
-# BugBug                 assert os.path.isfile(original_data_filename), original_data_filename
-# BugBug 
-# BugBug                 with open(original_data_filename) as f:
-# BugBug                     original_environment = json.load(f)
-# BugBug 
-# BugBug             for index, configuration in enumerate(configurations):
-# BugBug                 dm.stream.write("Configuration '{}' ({} of {})...".format( configuration if configuration != "None" else "<default>",
-# BugBug                                                                            index + 1,
-# BugBug                                                                            len(configurations),
-# BugBug                                                                          ))
-# BugBug                 with dm.stream.DoneManager() as this_dm:
-# BugBug                     if terminate:
-# BugBug                         continue
-# BugBug 
-# BugBug                     result_filename = shell.CreateTempFilename()
-# BugBug 
-# BugBug                     # ----------------------------------------------------------------------
-# BugBug                     def RemoveResultFilename():
-# BugBug                         if os.path.isfile(result_filename):
-# BugBug                             os.remove(result_filename)
-# BugBug 
-# BugBug                     # ----------------------------------------------------------------------
-# BugBug 
-# BugBug                     with CommonEnvironmentImports.CallOnExit(RemoveResultFilename):
-# BugBug                         commands = [ shell.Commands.EchoOff(),
-# BugBug                                      shell.Commands.Raw('cd "{}"'.format(os.path.dirname(activation_script))),
-# BugBug                                      shell.Commands.Call("{} {} /fast".format(os.path.basename(activation_script), configuration if configuration != "None" else '')),
-# BugBug                                      shell.Commands.ExitOnError(-1),
-# BugBug                                      shell.Commands.Augment("PYTHONPATH", _fundamental_root, update_memory=False),
-# BugBug                                      shell.Commands.Raw('python -m RepositoryBootstrap.Impl.Hooks.HookScript "{verb}" "{sentinel}" "{json_filename}" "{result_filename}"{first}' \
-# BugBug                                                           .format( verb=verb,
-# BugBug                                                                    sentinel=display_sentinel,
-# BugBug                                                                    json_filename=json_filename,
-# BugBug                                                                    result_filename=result_filename,
-# BugBug                                                                    first=" /first" if index == 0 else '',
-# BugBug                                                                  )),
-# BugBug                                      shell.Commands.ExitOnError(-1),
-# BugBug                                    ]
-# BugBug 
-# BugBug                         script_filename = shell.CreateTempFilename(shell.ScriptExtension)
-# BugBug                         with open(script_filename, 'w') as f:
-# BugBug                             f.write(shell.GenerateCommands(commands))
-# BugBug 
-# BugBug                         with CommonEnvironmentImports.CallOnExit(lambda: os.remove(script_filename)):
-# BugBug                             shell.MakeFileExecutable(script_filename)
-# BugBug 
-# BugBug                             content = []
-# BugBug                             
-# BugBug                             # ----------------------------------------------------------------------
-# BugBug                             def Display(value):
-# BugBug                                 if value.startswith(display_sentinel):
-# BugBug                                     stipped_value = value.replace(display_sentinel, '')
-# BugBug 
-# BugBug                                     this_dm.stream.write(stipped_value)
-# BugBug                                     this_dm.stream.flush()
-# BugBug 
-# BugBug                                 content.append(value)
-# BugBug 
-# BugBug                             # ----------------------------------------------------------------------
-# BugBug 
-# BugBug                             this_dm.result = CommonEnvironmentImports.Process.Execute( script_filename,
-# BugBug                                                                                        Display,
-# BugBug                                                                                        line_delimited_output=True,
-# BugBug                                                                                        environment=original_environment,
-# BugBug                                                                                      )
-# BugBug 
-# BugBug                             if is_debug:
-# BugBug                                 this_dm.stream.write(''.join(content))
-# BugBug 
-# BugBug                             if this_dm.result == -1:
-# BugBug                                 return this_dm.result
-# BugBug 
-# BugBug                             if not os.path.isfile(result_filename):
-# BugBug                                 raise Exception("The filename '{}' should have been generated by 'RepositoryBootstrap.Impl.Hooks.HookImpl' but it doesn't exist.".format(result_filename))
-# BugBug                             
-# BugBug                             with open(result_filename) as f:
-# BugBug                                 result = int(f.read().strip())
-# BugBug 
-# BugBug                             if result == -1:
-# BugBug                                 this_dm.result = result
-# BugBug                                 return this_dm.result
-# BugBug                             elif result == 1:
-# BugBug                                 pass                    # 1 is returned if a configuration was used
-# BugBug                             elif result == 0:
-# BugBug                                 terminate = True        # 0 is returned if a configuration was not used 
-# BugBug                             else:
-# BugBug                                 assert False, result
-# BugBug 
-# BugBug     return 0
+# ----------------------------------------------------------------------
+def _GetRepoRoot():
+    original_repo_root = os.getcwd()
+
+    repo_root = original_repo_root
+    while True:
+        if os.path.isdir(os.path.join(repo_root, ".hg")):
+            return repo_root
+
+        potential_repo_root = os.path.dirname(repo_root)
+        if potential_repo_root == repo_root:
+            raise Exception("No repository found in '{}' or its ancestors".format(original_repo_root))
+
+        repo_root = potential_repo_root
