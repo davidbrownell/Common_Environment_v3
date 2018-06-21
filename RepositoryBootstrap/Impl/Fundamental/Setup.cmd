27413f24489719b604255f257585be0b09916040
David Brownell <db@DavidBrownell.com>
2018-06-21 07:37:48 -0700

    Removed explicit echo off (as it is specified by scripts calling this one)

--- RepositoryBootstrap/Impl/Fundamental/Setup.cmd
+++ RepositoryBootstrap/Impl/Fundamental/Setup.cmd
@@ -12,7 +12,6 @@
 @REM |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 @REM |  
 @REM ----------------------------------------------------------------------
-@echo off
 
 echo.
 
