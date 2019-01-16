1bcb9159d0829ee6fb7585d790237b7e5c1feed7
David Brownell <db@DavidBrownell.com>
2019-01-15 19:12:25 -0800

    Added ignore functionality

--- Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/BlackAndBrown/Plugins/__init__.py
+++ Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/BlackAndBrown/Plugins/__init__.py
@@ -16,6 +16,7 @@
 """Contains the Plugin object"""
 
 import os
+import re
 
 import CommonEnvironment
 from CommonEnvironment import Interface
@@ -57,3 +58,99 @@ class Plugin(Interface.Interface):
     def Decorate(lines, *args, **kwargs):
         """Returns a list of decorated lines"""
         raise Exception("Abstract method")
+
+    # ----------------------------------------------------------------------
+    # |  Protected Methods
+    @classmethod
+    def EnumerateLines(cls, lines):
+        """Enumerates all non-ignored lines in the collection of lines."""
+        yield from cls._EnumerateLinesImpl(lines)
+
+    # ----------------------------------------------------------------------
+    @classmethod
+    def EnumerateBlocks(cls, lines):
+        for line_index, line, set_next_line_func in cls._EnumerateLinesImpl(
+            lines,
+            include_next_line_func=True,
+        ):
+            if not line.leaves:
+                continue
+
+            starting_line_index = line_index
+            line_index += 1
+
+            while line_index < len(lines) and lines[line_index].leaves:
+                line_index += 1
+
+            set_next_line_func(line_index)
+            yield starting_line_index, lines[starting_line_index:line_index]
+
+    # ----------------------------------------------------------------------
+    _IgnoreLine_comment_regex               = re.compile("^\s*#\s*(?P<prefix>.+?)\s*:\s*(?P<suffix>.+)\s*$")
+
+    @classmethod
+    def IgnoreLine(cls, line):
+        return line.comments and line.comments[0][1].value in ["# yapf: disable", "# fmt: off"]
+
+    # ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
+    @classmethod
+    def _EnumerateLinesImpl(
+        cls,
+        lines,
+        include_next_line_func=False,
+    ):
+        nonlocals = CommonEnvironment.Nonlocals(
+            index=0,
+        )
+
+        if include_next_line_func:
+            # ----------------------------------------------------------------------
+            def SetNextLine(index):
+                nonlocals.index = index
+
+            # ----------------------------------------------------------------------
+
+            yield_func = lambda index, line: (index, line, SetNextLine)
+        else:
+            yield_func = lambda index, line: (index, line)
+
+        disabled_command = None
+
+        while nonlocals.index < len(lines):
+            this_line = lines[nonlocals.index]
+            this_index = nonlocals.index
+            nonlocals.index += 1
+
+            if cls.IgnoreLine(this_line):
+                continue
+
+            comments = cls._GetComments(this_line)
+            if comments is not None:
+                if disabled_command is None:
+                    if "yapf: disable" in comments or "fmt: off" in comments:
+                        disabled_command = comments
+                        continue
+
+                elif ("yapf" in disabled_command and "yapf: enable" in comments) or ("fmt" in disabled_command and "fmt: on" in comments):
+                    disabled_command = None
+                    continue
+
+            if disabled_command is not None:
+                continue
+
+            yield yield_func(this_index, this_line)
+
+    # ----------------------------------------------------------------------
+    @staticmethod
+    def _GetComments(line):
+        # Gets the comments at the end of or within the provided line
+        if line.comments:
+            return line.comments[0][1].value
+
+        for leaf in line.leaves:
+            if leaf.value.startswith("#"):
+                return leaf.value
+
+        return None
