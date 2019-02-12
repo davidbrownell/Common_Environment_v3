0be9597add84cdf95be4fd935b3eb190d42e312a
David Brownell <db@DavidBrownell.com>
2019-02-12 14:11:37 -0800

    Updated type info serialization

--- Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/TypeInfo/FundamentalTypes/Serialization/JsonLikeSerialization.py
+++
@@ -1,4 +0,0 @@
-# <Unused import> pylint: disable = W0611
-
-# This is here for back-compat with v2. Once SimpleSchemaCodeGenerator plugins are updated, remove this file
-from CommonEnvironment.TypeInfo.FundamentalTypes.Serialization.JsonSerialization import JsonSerialization as JsonLikeSerialization
