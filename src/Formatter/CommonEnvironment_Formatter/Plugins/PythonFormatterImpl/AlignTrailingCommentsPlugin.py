6e40683673df40828fe56b5fcb3ceed84754e559
David Brownell <db@DavidBrownell.com>
2019-07-01 19:33:12 -0700

    Formatter updates (allow banned text)

--- src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignTrailingCommentsPlugin.py
+++ src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignTrailingCommentsPlugin.py
@@ -3,11 +3,11 @@
 # |  AlignTrailingCommentsPlugin.py
 # |
 # |  David Brownell <db@DavidBrownell.com>
-# |      2018-12-17 17:38:39
+# |      2019-06-29 14:43:48
 # |
 # ----------------------------------------------------------------------
 # |
-# |  Copyright David Brownell 2018
+# |  Copyright David Brownell 2019
 # |  Distributed under the Boost Software License, Version 1.0. See
 # |  accompanying file LICENSE_1_0.txt or copy at
 # |  http://www.boost.org/LICENSE_1_0.txt.
@@ -20,8 +20,8 @@ import os
 import CommonEnvironment
 from CommonEnvironment import Interface
 
-from AlignAssignmentsPlugin import Plugin as AlignAssignmentsPlugin
-from HorizontalAlignmentPluginImpl import HorizontalAlignmentPluginImpl
+from PythonFormatterImpl.AlignAssignmentsPlugin import Plugin as AlignAssignmentsPlugin
+from PythonFormatterImpl.Impl.HorizontalAlignmentImpl import HorizontalAlignmentImpl
 
 # ----------------------------------------------------------------------
 _script_fullpath                            = CommonEnvironment.ThisFullpath()
@@ -30,34 +30,36 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 
 # ----------------------------------------------------------------------
 @Interface.staticderived
-class Plugin(HorizontalAlignmentPluginImpl):
+class Plugin(HorizontalAlignmentImpl):
+    # ----------------------------------------------------------------------
+    # |  Properties
     Name                                    = Interface.DerivedProperty("AlignTrailingComments")
-    Priority                                = Interface.DerivedProperty(AlignAssignmentsPlugin.Priority + 1)
+    Priority                                = Interface.DerivedProperty(AlignAssignmentsPlugin().Priority + 1)
 
     # ----------------------------------------------------------------------
-    # ----------------------------------------------------------------------
-    # ----------------------------------------------------------------------
-    _AlignToLinesWithoutAlignmentLeaf       = Interface.DerivedProperty(True)
+    # |  Private Properties
+    _AlignToLinesWithoutAlignmentToken      = Interface.DerivedProperty(True)
 
     # ----------------------------------------------------------------------
+    # |  Private Methods
     @staticmethod
     @Interface.override
-    def _GetAlignmentLeaf(line, is_initial_line):
-        comment_leaf = None
+    def _GetAlignmentToken(line, is_initial_line):
+        comment_token = None
 
         if line.comments:
-            assert line.comments, line
-            assert isinstance(line.comments[0], tuple), line.comments[0]
+            # We only need to look at the first comment, as they are ordered left-to-right,
+            # and the first comment symbol means everything that comes after it is also
+            # a comment.
+            comment_token = line.comments[0][1]
 
-            comment_leaf = line.comments[0][1]
-
-        if comment_leaf is None:
-            for index, leaf in enumerate(line.leaves):
-                if getattr(leaf, "value", "").startswith("#") and (not is_initial_line or index != 0):
-                    comment_leaf = leaf
+        if comment_token is None:
+            for index, token in enumerate(line.leaves):
+                if token.value.startswith("#") and (not is_initial_line or index != 0):
+                    comment_token = token
                     break
 
-        if comment_leaf and comment_leaf.value.startswith("# BugBug"):
-            comment_leaf = None
+        if comment_token and comment_token.value.startswith("# BugBug"):
+            comment_token = None
 
-        return comment_leaf
+        return comment_token
