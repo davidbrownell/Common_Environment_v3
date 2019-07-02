498fd3988e3a847f03e396cb87d781094e398483
David Brownell <db@DavidBrownell.com>
2019-07-01 19:32:58 -0700

    Formatter updates

--- src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignAssignmentsPlugin.py
+++ src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignAssignmentsPlugin.py
@@ -3,11 +3,11 @@
 # |  AlignAssignmentsPlugin.py
 # |
 # |  David Brownell <db@DavidBrownell.com>
-# |      2018-12-17 21:18:10
+# |      2019-06-29 10:24:07
 # |
 # ----------------------------------------------------------------------
 # |
-# |  Copyright David Brownell 2018
+# |  Copyright David Brownell 2019
 # |  Distributed under the Boost Software License, Version 1.0. See
 # |  accompanying file LICENSE_1_0.txt or copy at
 # |  http://www.boost.org/LICENSE_1_0.txt.
@@ -17,10 +17,14 @@
 
 import os
 
+import black
+from blib2to3.pygram import python_symbols
+
 import CommonEnvironment
+from CommonEnvironment.BitFlagEnum import BitFlagEnum, auto
 from CommonEnvironment import Interface
 
-from HorizontalAlignmentPluginImpl import HorizontalAlignmentPluginImpl
+from PythonFormatterImpl.Impl.HorizontalAlignmentImpl import HorizontalAlignmentImpl
 
 # ----------------------------------------------------------------------
 _script_fullpath                            = CommonEnvironment.ThisFullpath()
@@ -28,93 +32,90 @@ _script_dir, _script_name                   = os.path.split(_script_fullpath)
 #  ----------------------------------------------------------------------
 
 # ----------------------------------------------------------------------
-@Interface.staticderived
-class Plugin(HorizontalAlignmentPluginImpl):
+class Plugin(HorizontalAlignmentImpl):
+    """Horizontally aligns assignment statements within a block"""
+
     # ----------------------------------------------------------------------
     # |  Types
-
-    # Not using Python3 enum to maintain compatibility with Python2
-    ModuleLevel                             = 1         # Align assignments at the module level
-    ClassLevel                              = 2         # Align assignments at the class level
-    InitLevel                               = 4         # Align self-based assignments in __init__ methods
-    InitAnyLevel                            = 8         # Align (any) assignments in __init__ methods
-    MethodLevel                             = 16        # Align assignments in modules (except __init__)
+    class Flag(BitFlagEnum):
+        ModuleLevel                         = auto()    # Align assignments at the module level
+        ClassLevel                          = auto()    # Align assignments at the class level
+        InitLevel                           = auto()    # Align self-based assignments in __init__ methods
+        InitAnyLevel                        = auto()    # Align any assignment in __init__ methods
+        MethodLevel                         = auto()    # Align assignments in methods other than __init__
 
     # ----------------------------------------------------------------------
     # |  Properties
     Name                                    = Interface.DerivedProperty("AlignAssignments")
-    Priority                                = Interface.DerivedProperty(HorizontalAlignmentPluginImpl.STANDARD_PRIORITY * 2)
+    Priority                                = Interface.DerivedProperty(HorizontalAlignmentImpl.STANDARD_PRIORITY)
 
     # ----------------------------------------------------------------------
-    # ----------------------------------------------------------------------
-    # ----------------------------------------------------------------------
-    _AlignToLinesWithoutAlignmentLeaf       = Interface.DerivedProperty(False)
-
-    # ----------------------------------------------------------------------
-    @classmethod
-    @Interface.override
-    def _GetAlignmentLeaf(
-        cls,
-        line,
-        is_initial_line,
-        flags=None,
+    # |  Methods
+    def __init__(
+        self,
+        alignment_flags=None,
+        *args,
+        **kwargs
     ):
-        # Importing here as black isn't supported by python2
-        import black
-        from blib2to3.pygram import python_symbols
+        if alignment_flags is None:
+            alignment_flags = (
+                self.Flag.ModuleLevel | self.Flag.ClassLevel | self.Flag.InitLevel
+            )
 
-        # <Instance of 'Symbols' has no 'parameters' member> pylint: disable = E1101
+        self._alignment_flags               = alignment_flags
 
-        if flags is None:
-            flags = cls.ModuleLevel | cls.ClassLevel | cls.InitLevel
+        super(Plugin, self).__init__(*args, **kwargs)
 
-        nested_parens = 0
-        nested_braces = 0
-        nested_brackets = 0
+    # ----------------------------------------------------------------------
+    # |  Private Properties
+    _AlignToLinesWithoutAlignmentToken      = Interface.DerivedProperty(False)
 
-        for leaf in line.leaves:
-            if leaf.value == "(":
-                nested_parens += 1
-            elif leaf.value == ")":
-                nested_parens -= 1
-            elif leaf.value == "{":
-                nested_braces += 1
-            elif leaf.value == "}":
-                nested_braces -= 1
-            elif leaf.value == "[":
-                nested_brackets += 1
-            elif leaf.value == "]":
-                nested_brackets -= 1
-            elif leaf.value == "=" and nested_parens == 0 and nested_braces == 0 and nested_brackets == 0:
-                node = leaf.parent
+    # ----------------------------------------------------------------------
+    # |  Private Methods
+    @Interface.override
+    def _GetAlignmentToken(self, line, is_initial_line):
+        tokens = line.leaves
 
-                while node:
-                    # Do not alter parameters
-                    if node.type in black.VARARGS_PARENTS or node.type in [python_symbols.parameters]:
-                        return None
+        assignment_index = self._GetRootTokenIndex(tokens, "=")
+        if assignment_index is None:
+            return None
 
-                    if node.type == python_symbols.classdef:
-                        return leaf if flags & cls.ClassLevel else None
+        token = tokens[assignment_index]
 
-                    if node.type == python_symbols.funcdef:
-                        # This code will be hit on the first leaf of a line
-                        # that is a function def. Look at the function
-                        # definition's name to determine what kind of method
-                        # it is.
-                        assert len(node.children) > 2, node.children
-                        if node.children[1].value == "__init__":
-                            if flags & cls.InitAnyLevel:
-                                return leaf
+        parent = token.parent
+        while parent:
+            if parent.type in black.VARARGS_PARENTS or parent.type in [
+                python_symbols.parameters
+            ]:
+                return None
 
-                            if flags & cls.InitLevel and cls.FirstLeafValue(line) == "self" and line.leaves[1].value == ".":
-                                return leaf
+            if parent.type == python_symbols.classdef:
+                return token if self._alignment_flags & self.Flag.ClassLevel else None
 
-                            return None
+            if parent.type == python_symbols.funcdef:
+                # This code will be hit on the first token of a line
+                # that is a function def. Look at the function definition's
+                # name to determine what kind of method it is.
+                assert len(parent.children) > 2, parent.children
+                if parent.children[1].value == "__init__":
+                    if self._alignment_flags & self.Flag.InitAnyLevel:
+                        return token
 
-                        return leaf if flags & cls.MethodLevel else None
+                    if (
+                        self._alignment_flags & self.Flag.InitLevel
+                        and len(tokens) > 2
+                        and tokens[0].value == "self"
+                        and tokens[1].value == "."
+                    ):
+                        return token
 
-                    node = node.parent
+                    return None
 
-                return leaf if flags & cls.ModuleLevel else None
+                else:
+                    return (
+                        token if self._alignment_flags & self.Flag.MethodLevel else None
+                    )
 
-        return None
+            parent = parent.parent
+
+        return token if self._alignment_flags & self.Flag.ModuleLevel else None
---
+++ src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/FunctionCallSplitterPlugin.py
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------------------
+# |
+# |  FunctionCallSplitterPlugin.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2019-06-29 09:53:09
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2019
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+"""Contains the Plugin object"""
+
+import os
+
+import black
+from blib2to3.pygram import python_symbols
+
+import CommonEnvironment
+from CommonEnvironment import Interface
+
+from PythonFormatterImpl.Impl.FunctionSplitterImpl import FunctionSplitterImpl
+
+# ----------------------------------------------------------------------
+_script_fullpath                            = CommonEnvironment.ThisFullpath()
+_script_dir, _script_name                   = os.path.split(_script_fullpath)
+#  ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+@Interface.staticderived
+class Plugin(FunctionSplitterImpl):
+    """Splits function calls that contain more then N args or have keyword arguments"""
+
+    # ----------------------------------------------------------------------
+    # |  Properties
+    Name                                    = Interface.DerivedProperty("FunctionCallSplitter")
+
+    # ----------------------------------------------------------------------
+    # |  Private Methods
+    @staticmethod
+    @Interface.override
+    def _IsInitialToken(token):
+        if not token.parent:
+            return False
+
+        if token.value != "(":
+            return False
+
+        return token.parent.type in [python_symbols.decorator, python_symbols.trailer]
