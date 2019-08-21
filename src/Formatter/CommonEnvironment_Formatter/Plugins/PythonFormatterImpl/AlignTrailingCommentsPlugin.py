611d0ea7c286b5fe647c747adef647a5e56f1f2e
David Brownell <db@DavidBrownell.com>
2019-08-21 07:36:19 -0700

    Better comment support for python formatting (Allow Banned text)

--- src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignTrailingCommentsPlugin.py
+++ src/Formatter/CommonEnvironment_Formatter/Plugins/PythonFormatterImpl/AlignTrailingCommentsPlugin.py
@@ -16,12 +16,20 @@
 """Contains the Plugin object"""
 
 import os
+import re
+import sys
+import textwrap
 
 import CommonEnvironment
 from CommonEnvironment import Interface
+from CommonEnvironment import RegularExpression
 
 from PythonFormatterImpl.AlignAssignmentsPlugin import Plugin as AlignAssignmentsPlugin # <Unable to import> pylint: disable = E0401
 from PythonFormatterImpl.Impl.HorizontalAlignmentImpl import HorizontalAlignmentImpl    # <Unable to import> pylint: disable = E0401
+
+if sys.version_info[0] != 2:
+    import black
+    from blib2to3.pygram import token as python_tokens
 
 # ----------------------------------------------------------------------
 _script_fullpath                            = CommonEnvironment.ThisFullpath()
@@ -40,14 +48,63 @@ class Plugin(HorizontalAlignmentImpl):
     # |  Public Methods
     @classmethod
     @Interface.override
+    def PreprocessContent(cls, content):
+        # black doesn't handle comments well, especially those embedded within a
+        # structure that would otherwise be split. Decorate comments that are
+        # header comments - those that are vertically aligned with content directly
+        # below it. Modify the contents so these special comments can be uniquely
+        # identified and then restored after black has altered the content.
+        regex = re.compile(
+            textwrap.dedent(
+               r"""(?#
+                Initial Whitespace                      )^(?P<whitespace>\s*)(?#
+                Comment                                 )(?P<content>#[^\r\n]*)(?#
+                Newline                                 )(?P<newline>\r?\n)(?#
+                Next Line Whitespace                    )(?P=whitespace)(?#
+                Non-comment, Non-whitespace char        )(?P<first_char>[^#\s])(?#
+                )""",
+            ),
+            re.MULTILINE,
+        )
+
+        # ----------------------------------------------------------------------
+        def OnMatch(match):
+            return "{ws}{comment}{newline}{ws}{first_char}".format(
+                comment=cls._HEADER_SUB_TEMPLATE.format(
+                    content=match.group("content"),
+                ),
+                newline=match.group("newline"),
+                ws=match.group("whitespace"),
+                first_char=match.group("first_char"),
+            )
+
+        # ----------------------------------------------------------------------
+
+        content = regex.sub(OnMatch, content)
+
+        return content
+
+    # ----------------------------------------------------------------------
+    @classmethod
+    @Interface.override
     def PreprocessLines(cls, lines):
         # We don't want the length of a comment to cause line wrapping in black. Remote the comment's
         # contents here, and then restore it once complete.
+
+        # ----------------------------------------------------------------------
+        def ProcessComment(comment):
+            if not hasattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME):
+                setattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME, comment.value)
+                comment.value = "#"
+
+        # ----------------------------------------------------------------------
+
         for line in lines:
+            if len(line.leaves) == 1 and line.leaves[0].type == black.STANDALONE_COMMENT:
+                ProcessComment(line.leaves[0])
+
             for _, comment in line.comments:
-                if not hasattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME):
-                    setattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME, comment.value)
-                    comment.value = "#"
+                ProcessComment(comment)
 
         return lines
 
@@ -56,17 +113,53 @@ class Plugin(HorizontalAlignmentImpl):
     @Interface.override
     def PostprocessLines(cls, lines):
         # Restore the original comments
-        for line in lines:
+
+        regex = RegularExpression.TemplateStringToRegex(cls._HEADER_SUB_TEMPLATE)
+
+        # ----------------------------------------------------------------------
+        def ProcessComment(comment):
+            if hasattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME):
+                comment.value = getattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME)
+                delattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME)
+
+            match = regex.match(comment.value)
+            if not match:
+                return False
+
+            comment.value = match.group("content")
+            return True
+
+        # ----------------------------------------------------------------------
+
+        line_index = 0
+        while line_index < len(lines):
+            line = lines[line_index]
+
+            if len(line.leaves) == 1 and line.leaves[0].type in [black.STANDALONE_COMMENT, python_tokens.COMMENT]:
+                ProcessComment(line.leaves[0])
+
             for _, comment in line.comments:
-                if hasattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME):
-                    comment.value = getattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME)
-                    delattr(comment, cls._ORIGINAL_VALUE_ATTRIBUTE_NAME)
+                if ProcessComment(comment):
+                    # If here, this trailing comment is a header comment on its own line
+                    # prior to this one.
+                    assert len(line.comments) == 1, len(line.comments)
+                    new_line = black.Line()
+                    new_line.depth = line.depth
+
+                    new_line.leaves.append(line.comments.pop(0)[1])
+                    new_line.leaves[0].prefix = ""
+
+                    lines.insert(line_index, new_line)
+
+            line_index += 1
 
         return lines
 
     # ----------------------------------------------------------------------
     # |  Private Properties
     _ORIGINAL_VALUE_ATTRIBUTE_NAME          = "_align_trailing_comment_original_value"
+
+    _HEADER_SUB_TEMPLATE                    = "# __HeaderComment<<??!!__: {content}"
 
     _AlignToLinesWithoutAlignmentToken      = Interface.DerivedProperty(True)
 
