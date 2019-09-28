9ec2379658eb06084b7858db038784372d00ebed
David Brownell <db@DavidBrownell.com>
2019-09-27 22:59:35 -0700

    Fixed test break

--- Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Shell/Commands/UnitTests/Visitor_UnitTest.py
+++ Libraries/Python/CommonEnvironment/v1.0/CommonEnvironment/Shell/Commands/UnitTests/Visitor_UnitTest.py
@@ -1,16 +1,16 @@
 # ----------------------------------------------------------------------
-# |  
+# |
 # |  Visitor_UnitTest.py
-# |  
+# |
 # |  David Brownell <db@DavidBrownell.com>
 # |      2018-04-30 19:18:51
-# |  
+# |
 # ----------------------------------------------------------------------
-# |  
+# |
 # |  Copyright David Brownell 2018-19.
 # |  Distributed under the Boost Software License, Version 1.0.
 # |  (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-# |  
+# |
 # ----------------------------------------------------------------------
 """Unit test for CommandVisitor.py"""
 
@@ -86,9 +86,9 @@ class StandardSuite(unittest.TestCase):
         visitor.Accept(Call("command line"))
         visitor.Accept(Execute("command line"))
         visitor.Accept(SymbolicLink("link", "target", False))
-        visitor.Accept(Path("path", True))
+        visitor.Accept(Path("path"))
         visitor.Accept(AugmentPath("path"))
-        visitor.Accept(Set("name", "value", True))
+        visitor.Accept(Set("name", "value"))
         visitor.Accept(Augment("name", "value"))
         visitor.Accept(Exit(False, False, 0))
         visitor.Accept(ExitOnError(-1))
