e81e8d407ec23deb6af5e13e414dc8fdd3b1ff43
David Brownell <db@DavidBrownell.com>
2020-09-10 16:51:37 -0700

    Fixed typo

--- Activate_custom.py
+++ Activate_custom.py
@@ -254,7 +254,7 @@ def GetCustomActions(
 
                                 It is recommended that you change this setting to not modify line endings:
 
-                                    1) 'git config --global core.autocrlf false`
+                                    1) 'git config --global core.autocrlf false'
 
                                 # ----------------------------------------------------------------------
                                 # ----------------------------------------------------------------------
